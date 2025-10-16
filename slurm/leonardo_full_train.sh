3f3a312312521b830551585850d171926b40bde8
David Hauser <hauser-david@hotmail.com>
2025-10-16 22:22:25 +0200

    fix

--- slurm/leonardo_full_train.sh
+++ slurm/leonardo_full_train.sh
@@ -7,9 +7,9 @@
 #SBATCH --partition=boost_usr_prod
 #SBATCH --qos=boost_qos_lprod
 #SBATCH --time=48:00:00
-#SBATCH --chdir=/leonardo_scratch/fast/EUHPC_D18_005/david/FastV/src/LLaVA
-#SBATCH --output=/leonardo_scratch/fast/EUHPC_D18_005/david/outputs/fastv.out
-#SBATCH --error=/leonardo_scratch/fast/EUHPC_D18_005/david/outputs/fastv.err
+#SBATCH --chdir=/leonardo_scratch/fast/EUHPC_D18_005/david/relora-3000
+#SBATCH --output=/leonardo_scratch/fast/EUHPC_D18_005/david/outputs/fulltrain_relora.out
+#SBATCH --error=/leonardo_scratch/fast/EUHPC_D18_005/david/outputs/fulltrain_relora.err
 
 # See running jobs
 # watch -n 1 squeue --me
